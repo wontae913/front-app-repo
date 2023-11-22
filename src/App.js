2b2bd414325255ef2b83dee32007da704bb3c0b2
wontae913 <wontae0913@gmail.com>
2023-11-22 05:59:55 +0000

    new blog version

--- src/App.js
+++ src/App.js
@@ -64,7 +64,7 @@ function App(props) {
             align="center"
             className={classes.title}
           >
-            EKS DEMO Blog version 12
+            EKS DEMO Blog version 12asdfasdfasdfasdf
           </Typography>
           {new Date().toLocaleTimeString()}
         </Toolbar>
